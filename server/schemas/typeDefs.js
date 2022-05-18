9c6741aaef5946044906fdcb9496aefdd56075ca
Robert Bullfrog <williampikus@gmail.com>
2022-05-17 20:31:42 -0700

    File structure updated

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Category {
+    _id: ID
+    name: String
+    image: String
+    description: String
+  }
+
+  type Service {
+    _id: ID
+    name: String
+    price: Float
+    category: Category
+  }
+
+  type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
