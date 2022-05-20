39a35f222d2ccae721ac6ddccab1f39394e5a859
Robert Bullfrog <williampikus@gmail.com>
2022-05-19 19:42:07 -0700

    Type defs and resolvers complete

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Service, User, Category } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    categories: async () => {
+        return await Category.find();
+    },
+    // User Queries
+    // get all users
+    users: async () => {
+        return await User.find();
+    },
+     // User by ID
+    user: async (parent, { userId }) => {
+    return User.findOne({ _id: userId });
+    },
+    // 
+    services: async () => {
+        return await Service.find().populate('category');
+    },
+    service: async (parent, { serviceId }) => {
+        return Service.findOne({ _id: serviceId }).populate('category');
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+   
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+   
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  }
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -20,7 +20,6 @@ const typeDefs = gql`
     firstName: String
     lastName: String
     email: String
-    orders: [Order]
   }
 
   type Checkout {
@@ -34,18 +33,15 @@ const typeDefs = gql`
 
   type Query {
     categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
-    user: User
-    order(_id: ID!): Order
-    checkout(products: [ID]!): Checkout
+    users: [User]
+    user(userId: ID!): User
+    services: [Service]
+    service(serviceId: ID!): Service
   }
 
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addOrder(products: [ID]!): Order
     updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
   }
 `;
